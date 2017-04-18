a8b13bf3cde33295c06c52493dac1d3bfbf47573
stephHiyagon <mv.hiyagon@gmail.com>
2017-04-18 17:56:59 -0500

    subiendo carpetas

---
+++ js/app.js
@@ -0,0 +1,7 @@
+window.addEventListener('load',function(){
+  var body=document.getElementsByTagName('body')[0];
+  var title=document.createElement("h3");
+  var text=document.createTextNode("Nuestras coders");
+  title.appendChild(text);
+  body.appendChild(title);
+})
