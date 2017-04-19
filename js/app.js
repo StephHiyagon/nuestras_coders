06e9e3eb19b92bd0e32598a7de2ce9c894409286
stephHiyagon <mv.hiyagon@gmail.com>
2017-04-19 11:13:40 -0500

    cambios en css, tipografía y opacidad de las imágenes

--- js/app.js
+++ js/app.js
@@ -1,22 +1,14 @@
-window.addEventListener('load',function(){
+window.addEventListener("DOMContentLoaded",function(){
   var body=document.getElementsByTagName('body')[0];
   var section1=document.createElement('section');
   section1.setAttribute('id','principal');
-  var title=document.createElement("h3");
+  var title=document.createElement("h2");
   var text=document.createTextNode("Nuestras coders");
   var divNegro=document.createElement("div");
   divNegro.setAttribute('id','negro');
   var textNegro=document.createTextNode("-");
-  var coders=[
-    {nombre: "Fiorella Quispe"},
-    {nombre:"Lourdes Vilchez"},
-    {nombre:"Rossmery Maldonado"},
-    {nombre:"Yessenia Huaman"},
-    {nombre:"Miriam Mendoza"},
-    {nombre:"Elizabeth Condori"},
-  ];
+  var coders=[{nombre: "Fiorella Quispe"},{nombre:"Lourdes Vilchez"},{nombre:"Rossmery Maldonado"},{nombre:"Yessenia Huaman"},{nombre:"Miriam Mendoza"},{nombre:"Elizabeth Condori"}];
   var tamano=coders.length;
-  console.log(tamano);
   var section2=document.createElement('section');
   section2.setAttribute('class','fotos');
   var fragment=document.createDocumentFragment();
@@ -25,7 +17,6 @@ window.addEventListener('load',function(){
     divFoto.setAttribute('class','coder');
     var img=document.createElement('img');
     img.src="images/" + coders[i].nombre + ".png";
-    console.log(img.src);
     img.alt= coders[i].nombre;
     var span=document.createElement('span');
     span.innerHTML=coders[i].nombre;
@@ -33,9 +24,8 @@ window.addEventListener('load',function(){
     divFoto.appendChild(img);
     divFoto.appendChild(span);
     fragment.appendChild(divFoto);
-    section2.appendChild(fragment);
-  }
-
+    }
+  section2.appendChild(fragment);
   divNegro.appendChild(textNegro);
   title.appendChild(text);
   section1.appendChild(title);
@@ -43,33 +33,3 @@ window.addEventListener('load',function(){
   section1.appendChild(section2);
   body.appendChild(section1);
 })
-
-// function grid(){
-//   var coders=[
-//     {nombre: "Fiorella Quispe"},
-//     {nombre:"Lourdes Vilchez"},
-//     {nombre:"Rossmery Maldonado"},
-//     {nombre:"Yessenia Huaman"},
-//     {nombre:"Miriam Mendoza"},
-//     {nombre:"Elizabeth Condori"},
-//   ];
-//   var tamano=coders.length;
-//   console.log(tamano);
-//   var section2=document.createElement('section');
-//   section2.setAttribute('class','fotos');
-//   for(var i=0; i< tamano;i++){
-//     var divFoto=document.createElement('div');
-//     divFoto.setAttribute('class','coder');
-//     var img=document.createElement('img');
-//     img.src="images/" + coders[i].nombre + ".png";
-//     console.log(img.src);
-//     img.alt= coders[i].nombre;
-//     var span=document.createElement('span');
-//     span.innerHTML=coders[i].nombre;
-//
-//     divFoto.appendChild(img);
-//     divFoto.appendChild(span);
-//     section2.appendChild(divFoto);
-//   }
-//     section1.appendChild(section2);
-// }
